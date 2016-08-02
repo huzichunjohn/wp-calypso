1853faeeb3bf47761b108a4f06e9cebbc3fe90da
Dennis Snell <dennis.snell@automattic.com>
2016-08-02 09:56:34 -0700

    SEO Preview: Discern post vs. site preview
    Previously, every preview in the SEO preview pane was showing both a
    site and a post preview because it was unaware if we were viewing from
    somewhere like **My Sites** instead of from within the **Post Editor**.
    
    Now, the preview component detects if the post editor is currently open
    and if it is will fill in the `post` prop to the `<SeoPreviewPane />`
    inside of `connect()`

--- client/components/seo-preview-pane/index.jsx
+++ client/components/seo-preview-pane/index.jsx
@@ -17,7 +17,14 @@ import TwitterPreview from 'components/seo/twitter-preview';
 import SearchPreview from 'components/seo/search-preview';
 import VerticalMenu from 'components/vertical-menu';
 import { SocialItem } from 'components/vertical-menu/items';
-import { getSelectedSite } from 'state/ui/selectors';
+import { getEditorPostId } from 'state/ui/editor/selectors';
+import { getSitePost } from 'state/posts/selectors';
+import {
+	getSectionName,
+	getSelectedSite
+} from 'state/ui/selectors';
+
+const largeBlavatar = site => `${ get( site, 'icon.img', '//gravatar.com/avatar/' ) }?s=512`;
 
 const ComingSoonMessage = translate => (
 	<div className="seo-preview-pane__message">
@@ -25,51 +32,60 @@ const ComingSoonMessage = translate => (
 	</div>
 );
 
-const GooglePreview = site =>
+const GoogleSite = site => (
 	<SearchPreview
 		title={ site.name }
 		url={ site.URL }
 		snippet={ site.description }
-	/>;
-
-const PreviewFacebook = site => (
-	<div>
-		<FacebookPreview
-			title={ site.name }
-			url={ site.URL }
-			type="website"
-			description={ site.description }
-			image={ `${ get( site, 'icon.img', '//gravatar.com/avatar/' ) }?s=512` }
-		/>
-		<div style={ { marginBottom: '2em' } } />
-		<FacebookPreview
-			title={ site.name }
-			url={ site.URL }
-			type="article"
-			description={ site.description }
-			image={ `${ get( site, 'icon.img', '//gravatar.com/avatar/' ) }?s=512` }
-		/>
-	</div>
+	/>
 );
 
-const PreviewTwitter = site => (
-	<div>
-		<TwitterPreview
-			title={ site.name }
-			url={ site.URL }
-			type="summary"
-			description={ site.description }
-			image={ `${ get( site, 'icon.img', '//gravatar.com/avatar/' ) }?s=512` }
-		/>
-		<div style={ { marginBottom: '2em' } } />
-		<TwitterPreview
-			title={ site.name }
-			url={ site.URL }
-			type="large_image_summary"
-			description={ site.description }
-			image={ `${ get( site, 'icon.img', '//gravatar.com/avatar/' ) }?s=512` }
-		/>
-	</div>
+const GooglePost = ( site, post ) => (
+	<SearchPreview
+		title={ post.title }
+		url={ post.URL }
+		snippet={ post.excerpt || post.content }
+	/>
+);
+
+const FacebookSite = site => (
+	<FacebookPreview
+		title={ site.name }
+		url={ site.URL }
+		type="website"
+		description={ site.description }
+		image={ largeBlavatar( site ) }
+	/>
+);
+
+const FacebookPost = ( site, post ) => (
+	<FacebookPreview
+		title={ post.title }
+		url={ post.URL }
+		type="article"
+		description={ post.excerpt || post.content }
+		image={ post.featured_image || largeBlavatar( site ) }
+	/>
+);
+
+const TwitterSite = site => (
+	<TwitterPreview
+		title={ site.name }
+		url={ site.URL }
+		type="summary"
+		description={ site.description }
+		image={ largeBlavatar( site ) }
+	/>
+);
+
+const TwitterPost = ( site, post ) => (
+	<TwitterPreview
+		title={ post.title }
+		url={ post.URL }
+		type="large_image_summary"
+		description={ post.excerpt || post.content }
+		image={ post.featured_image || largeBlavatar( site ) }
+	/>
 );
 
 export class SeoPreviewPane extends PureComponent {
@@ -89,6 +105,7 @@ export class SeoPreviewPane extends PureComponent {
 
 	render() {
 		const {
+			post,
 			site,
 			translate
 		} = this.props;
@@ -118,10 +135,15 @@ export class SeoPreviewPane extends PureComponent {
 				</div>
 				<div className="seo-preview-pane__preview-area">
 					<div className="seo-preview-pane__preview">
-						{ get( {
-							facebook: PreviewFacebook( site ),
-							google: GooglePreview( site ),
-							twitter: PreviewTwitter( site )
+						{ post && get( {
+							facebook: FacebookPost( site, post ),
+							google: GooglePost( site, post ),
+							twitter: TwitterPost( site, post )
+						}, selectedService, ComingSoonMessage( translate ) ) }
+						{ ! post && get( {
+							facebook: FacebookSite( site ),
+							google: GoogleSite( site ),
+							twitter: TwitterSite( site )
 						}, selectedService, ComingSoonMessage( translate ) ) }
 					</div>
 					<div className="seo-preview-pane__preview-spacer" />
@@ -131,12 +153,15 @@ export class SeoPreviewPane extends PureComponent {
 	}
 }
 
-SeoPreviewPane.propTypes = {
-	type: PropTypes.oneOf( [ 'site', 'page', 'post' ] ).isRequired
-};
+const mapStateToProps = state => {
+	const site = getSelectedSite( state );
+	const postId = getEditorPostId( state );
+	const isEditorShowing = 'post-editor' === getSectionName( state );
 
-const mapStateToProps = state => ( {
-	site: getSelectedSite( state )
-} );
+	return {
+		site: site,
+		post: isEditorShowing && getSitePost( state, site.ID, postId )
+	}
+};
 
 export default connect( mapStateToProps, null )( localize( SeoPreviewPane ) );
