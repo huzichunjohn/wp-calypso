5d7f8a05ff2e15d34aace0dd662c428daf66a10e
Bernhard Reiter <ockham@raz.or.at>
2016-08-04 19:50:06 +0200

    document-head selectors: Add JSDoc comments

--- client/state/document-head/selectors.js
+++ client/state/document-head/selectors.js
@@ -1,7 +1,20 @@
+/**
+ * Returns the document title as set by the DocumentHead component or setTitle
+ * action.
+ *
+ * @param  {Object}  state  Global state tree
+ * @return {?String}        Document title
+ */
 export function getTitle( state ) {
 	return state.documentHead.title;
 }
 
+/**
+ * Returns a count reflecting unread items.
+ *
+ * @param  {Object}  state  Global state tree
+ * @return {?String}        Unread count (string because it can be e.g. '40+')
+ */
 export function getUnreadCount( state ) {
 	return state.documentHead.unreadCount;
 }
