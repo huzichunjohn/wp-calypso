48193a8bf938d3ad606eec124808dbc0d1aa734c
Bernhard Reiter <ockham@raz.or.at>
2016-08-04 21:22:34 +0200

    DocumentTitle: Remove middleware, add getFormattedTitle selector, subscribe document.title changing to store

--- client/state/document-head/selectors.js
+++ client/state/document-head/selectors.js
@@ -1,4 +1,15 @@
 /**
+ * External dependencies
+ */
+import { includes } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import { decodeEntities } from 'lib/formatting';
+import { getSelectedSite, getGroupName } from 'state/ui/selectors';
+
+/**
  * Returns the document title as set by the DocumentHead component or setTitle
  * action.
  *
@@ -17,4 +28,48 @@ export function getTitle( state ) {
  */
 export function getUnreadCount( state ) {
 	return state.documentHead.unreadCount;
+}
+
+export function getFormattedTitle( state ) {
+	const siteSpecificGroups = [ 'sites', 'editor' ];
+	const title = getTitle( state );
+	const unreadCount = getUnreadCount( state );
+	const site = getSelectedSite( state );
+
+	let pageTitle = '';
+
+	if ( unreadCount ) {
+		pageTitle += '(' + unreadCount + ') ';
+	}
+
+	pageTitle += title;
+
+	// Display site name as title part only if we're in 'My Sites'
+	if ( includes( siteSpecificGroups, getGroupName( state ) ) && site ) {
+		pageTitle = appendSite( pageTitle, site );
+	}
+
+	if ( pageTitle ) {
+		pageTitle = decodeEntities( pageTitle ) + ' — WordPress.com';
+	} else {
+		pageTitle = 'WordPress.com';
+	}
+
+	return pageTitle;
+}
+
+function appendSite( title, site ) {
+	let siteName;
+
+	if ( site.name ) {
+		siteName = site.name;
+	} else {
+		siteName = site.domain;
+	}
+
+	if ( title ) {
+		return title + ' \u2039 ' + siteName;
+	}
+
+	return siteName;
 }
